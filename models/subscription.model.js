0f9bf9f1f8aebe7d4230ecb88c6896fe9c60fac1
Keaton Nordby <keatonnordby4@gmail.com>
2025-02-27 17:29:29 -0600

    Fixed MongoDB connection issue

---
+++ models/subscription.model.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Subscription name is required'],
+        trim: true,
+        minLength: 2,
+        maxLength: 100
+    },
+    price: {
+        type: Number,
+        required: [true, 'Subscription price is required'],
+        min: [0, 'Price must be greater than 0']
+    },
+    currency: {
+        type: String,
+        enum: ['USD', 'EUR', 'GBT'],
+        default: 'USD'
+    },
+    frequency: {
+        type: String,
+        enum: ['daily', 'weekly', 'monthly', 'yearly']
+    },
+    category: {
+        type: String,
+        enum: ['sports', 'news', 'entertainment', 'lifestyle', 'technology', 'finance', 'politics', 'others'],
+        required: true
+    },
+    paymentMethod: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    status: {
+        type: String,
+        enum: ['active', 'cancelled', 'expired'],
+        default: 'active'
+    }
+
+}, {timestamps: true });
